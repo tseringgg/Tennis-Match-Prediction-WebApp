b0ade488ac1847b93bc92a8f6bf37d53a7d81b2b
tseringgg <jorjei.ngoche@hotmail.com>
2024-11-17 14:32:30 -0800

    displays mock matches in production

--- tennis-mp-client/src/app/data/match-data.service.ts
+++ tennis-mp-client/src/app/data/match-data.service.ts
@@ -1,31 +1,41 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Config } from '../models/config-model';
+import { of } from 'rxjs';
+import { MOCK_MATCHES } from './mock-matches';
 import { Match } from '../models/match-model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class MatchDataService {
-  //url = 'assets/test.json';
-  url = 'http://localhost:5146/api/MatchRecords'
-  constructor(private http: HttpClient) { 
-    
-  }
+  url = 'http://localhost:5146/api/MatchRecords';
+  
+  constructor(private http: HttpClient) { }
 
   getMatches() {
-    let result = this.http.get<any[]>(this.url, {observe: 'response'});
+    if (environment.production) {
+      console.log('Returning mock matches in production');
+      let result = of(({ body: MOCK_MATCHES, headers: {} } as any) as any as import('@angular/common/http').HttpResponse<any[]>);
+      return result;
+    }
+    let result = this.http.get<any[]>(this.url, { observe: 'response' });
     console.log(result);
     return result;
-    // For HTTP Params:
-    // https://v17.angular.io/guide/http-configure-http-url-parameters
-    // example: api/heroes?name=cat
   }
 
   getMatchesBetween(player1: string, player2: string, pageSize: number, pageIndex: number) {
-    //let result = this.http.get<any[]>(this.url+'/matches-between/'+player1+'/'+player2, {observe: 'response'})
-    let result = this.http.get<any[]>(`${this.url}/matches-between/${player1}/${player2}?pageSize=${pageSize}&pageNumber=${pageIndex}`, {observe: 'response'})
+    if (environment.production) {
+      console.log('Returning mock matches between players in production');
+      let filteredMatches = MOCK_MATCHES.filter(match => 
+        (match.p0_name === player1 && match.p1_name === player2) || 
+        (match.p0_name === player2 && match.p1_name === player1)
+      );
+      let result = of(({ body: filteredMatches, headers: {} } as any) as any as import('@angular/common/http').HttpResponse<any[]>);
+      return result;
+    }
+    let result = this.http.get<any[]>(`${this.url}/matches-between/${player1}/${player2}?pageSize=${pageSize}&pageNumber=${pageIndex}`, { observe: 'response' });
     console.log(result);
     return result;
   }
---
+++ tennis-mp-client/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://localhost:5146/api/MatchRecords'
+};
